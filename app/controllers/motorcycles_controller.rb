6ea4cd710664cf22e960434b753150040db56293
TJ Zuvic <terrance@zuvmarketing.com>
2020-04-30 00:01:12 -0400

    Re completed searchbar form, motorcycles#index and index.html.erb view

--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -1,5 +1,17 @@
 class MotorcyclesController < ApplicationController
+
+  def index
+    @motorcycles = Motorcycle.all
+    @search = params["search"]
+      if @search.present?
+        @description = @search["description"]
+        @motorcycles = Motorcycle.where(description: @description)
+      end
+    end
+
+
   def show
 
   end
+
 end
