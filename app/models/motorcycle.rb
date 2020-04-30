ecb71cbae732d3d23e77a7bc159f7c6ee13ec8fe
Victor Chiang <vgchiang@gmail.com>
2020-04-30 16:30:21 -0500

    updated the new page and created the .env file

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,3 +1,6 @@
 class Motorcycle < ApplicationRecord
   belongs_to :user
+
+  validates :description, :brand, :model, :year, :price_per_day, :motorcycle_type, presence: true
+  validates :engine_size, :title, presence: true
 end
