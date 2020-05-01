72d1ddadc3fa57b418665e8dc14fb6e4d5bd95ad
TJ Zuvic <terrance@zuvmarketing.com>
2020-04-30 23:45:09 -0400

    Updated Motorcycles Controller Action Index for city instead of description

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,11 @@ motorcycle_types = ["Sport", "Touring", "Dual-Sport", "Dirtbike", "Street", "Cru
 engine_sizes = [1000, 600, 800, 400, 250,]
 adjectives = ["Amazing", "Fantastic", "Incredible", "Badass", "Chingon"]
 
+
 Motorcycle.delete_all
 
 10.times do
   user = User.create(email: Faker::Internet.email, password:'password' , password_confirmation: 'password')
-  motorcycle = Motorcycle.create(user: user, brand: motorcycle_makes.sample, motorcycle_type: motorcycle_types.sample, model: motorcycle_models.sample, engine_size: engine_sizes.sample, price_per_day: rand(50..200), description: "Wow what a great motorcycle", year: rand(1975..2020), title: "#{adjectives.sample} New Motorcycle For Rent")
+  motorcycle = Motorcycle.create(user: user, brand: motorcycle_makes.sample, motorcycle_type: motorcycle_types.sample, model: motorcycle_models.sample, engine_size: engine_sizes.sample, price_per_day: rand(50..200), description: "Wow what a great motorcycle", year: rand(1975..2020), title: "#{adjectives.sample} New Motorcycle For Rent", city: "Mexico City")
   puts "#{motorcycle.model}  Added!"
 end
