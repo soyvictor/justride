6587e85232d7bc771ee9fae6ed39eef963166921
TJ Zuvic <terrance@zuvmarketing.com>
2020-05-01 22:16:44 -0400

    Changed city column name to address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_033702) do
+ActiveRecord::Schema.define(version: 2020_05_02_021452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2020_05_01_033702) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
-    t.string "city"
+    t.string "address"
     t.index ["user_id"], name: "index_motorcycles_on_user_id"
   end
 
