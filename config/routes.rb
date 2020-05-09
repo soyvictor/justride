2a2f51a71371b2189c45202a1337c363d4f9486f
TJ Zuvic <terrance@zuvmarketing.com>
2020-05-09 12:38:52 -0400

    Added Review Div to Show View

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :motorcycles do
     resources :bookings, only: [:create]
+    resources :reviews, only: :create
     collection do
       get :my_motorcycles, :my_motorcycles_host
     end
